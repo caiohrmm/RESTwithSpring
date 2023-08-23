430a7a50e657e4744c35862a8a643739fe52025e
caiohrmm <cahenromar@gmail.com>
2023-08-23 15:27:12 -0300

    Adicionando Content Negociations - XML, YML e JSON

--- src/main/java/br/com/caiohenrique/controllers/PersonController.java
+++ src/main/java/br/com/caiohenrique/controllers/PersonController.java
@@ -3,11 +3,14 @@ package br.com.caiohenrique.controllers;
 import br.com.caiohenrique.data.vo.v1.PersonVO;
 import br.com.caiohenrique.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static br.com.caiohenrique.util.MediaType.APPLICATION_JSON;
+import static br.com.caiohenrique.util.MediaType.APPLICATION_XML;
+import static br.com.caiohenrique.util.MediaType.APPLICATION_YML;
 
 // Preciso sempre da anotação do Spring para reconhecer que essa é uma classe de controle do REST.
 @RestController
@@ -17,22 +20,22 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = { APPLICATION_JSON, APPLICATION_XML, APPLICATION_YML})
     public List<PersonVO> findAllPersons() {
         return service.findAllPersons();
     }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = { APPLICATION_JSON, APPLICATION_XML, APPLICATION_YML})
     public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception {
         return service.findById(id);
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = { APPLICATION_JSON, APPLICATION_XML, APPLICATION_JSON}, produces = { APPLICATION_JSON, APPLICATION_XML, APPLICATION_YML})
     public PersonVO createPerson(@RequestBody PersonVO personVO) {
         return service.createPerson(personVO);
     }
 
-    @PutMapping (consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(consumes = { APPLICATION_JSON, APPLICATION_XML, APPLICATION_JSON}, produces = { APPLICATION_JSON, APPLICATION_XML, APPLICATION_YML})
     public PersonVO updatePerson(@RequestBody PersonVO personVO) {
         return service.updatePerson(personVO);
     }
